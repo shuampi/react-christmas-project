bdd78e2a1823d9aee79c03c3946cb4eba54b89a4
shuamp <castel.davila@gmail.com>
2022-01-17 20:11:53 +0000

    second commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import HeroStatusBar from '../herosStatusBar/index';
+function App() {
+  return (
+    <div className="App">
+     hello juan
+<HeroStatusBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/herosStatusBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeroStatusBar(){
+    
+return (<div>
+    <h1>Hero's Journey</h1>
+</div>)
+
+}
+
+export default HeroStatusBar;
